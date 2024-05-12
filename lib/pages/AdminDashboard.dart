d33c1ed81baea060e347181c654c71c826ee79b2
alkanozge <126454491+alkanozge@users.noreply.github.com>
2024-05-12 17:21:38 +0300

    Update AdminDashboard.dart

--- lib/pages/AdminDashboard.dart
+++ lib/pages/AdminDashboard.dart
@@ -1,4 +1,8 @@
+import 'package:enhanced_course_feedback_system/pages/StudentWriteFeedbackPage.dart';
 import 'package:flutter/material.dart';
+
+import 'ChatGPTConfigPage.dart';
+import 'EmailServerConfigPage.dart';
 
 class AdminDashboard extends StatefulWidget {
   const AdminDashboard({super.key});
@@ -10,6 +14,48 @@ class AdminDashboard extends StatefulWidget {
 class _AdminDashboardState extends State<AdminDashboard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin Dashboard'),
+        backgroundColor: Colors.indigo.shade100,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatGPTConfigPage(settings: ['Setting 1', 'Setting 2', 'Setting 3','Setting 1', 'Setting 2', 'Setting 3','Setting 1', 'Setting 2', 'Setting 3','Setting 1', 'Setting 2', 'Setting 3'],),
+                      ),
+                  );
+                }, 
+                child: const Text('Configure ChatGPT API'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(300, 70),
+              ),
+
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => EmailServerConfigPage(),
+                      ),
+                  );
+                },
+                child: const Text('Configure E-Mail Server'),
+                style: ElevatedButton.styleFrom(
+                minimumSize: Size(300, 70),
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
