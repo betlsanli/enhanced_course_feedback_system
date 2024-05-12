bd25bc0ff3d716df236a961c82dfef6d80f05efa
alkanozge <126454491+alkanozge@users.noreply.github.com>
2024-05-12 21:20:25 +0300

    Update StudentWriteFeedbackPage.dart

--- lib/pages/StudentWriteFeedbackPage.dart
+++ lib/pages/StudentWriteFeedbackPage.dart
@@ -19,150 +19,121 @@ class _StudentWriteFeedbackPageState extends State<StudentWriteFeedbackPage>{
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Course Feedback'),
-        backgroundColor: Colors.indigo.shade300,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(  //gives an overflow error without this
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 16),
-              Container(
-                padding: EdgeInsets.all(16.0),
-                width: 400.0,
-                height: 180,
-                decoration: BoxDecoration(
-                  color: Colors.indigo.shade50,
-                  borderRadius: BorderRadius.circular(8.0),
-
-                ),
-
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 10),
-                    Text(
-                      'Course Code: ${widget.requestForm.code}',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Course Name: ${widget.requestForm.name}',
-                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    Text('Topic: ${widget.requestForm.topic}'),
-                    SizedBox(height: 20),
-
-                    //Text('Teacher: ${widget.requestForm.teacherName}'),
-                    //SizedBox(height: 8),
-
-                    Text('Date: ${widget.requestForm.date}'),
-                  ],
-                ),
-              ),
-
-              SizedBox(height: 70),
-
-              Text(
-                'Share your feedback:',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16),
-
-
-              TextField(
-                controller: feedbackController,
-                minLines: 5,
-                maxLines: 10, //buna bak
-                decoration: InputDecoration(
-                  hintText: 'Enter your feedback here.',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-
-            /*
-              GestureDetector(
-                onVerticalDragUpdate: (details) {
-                  // Update the height of the text field based on drag gesture
-                  setState(() {
-                    _textFieldHeight += details.primaryDelta!;
-                  });
-                },
-                child: AnimatedContainer(
-                  height: _textFieldHeight,
-                  duration: Duration(milliseconds: 200),
-                  curve: Curves.easeInOut,
-                  padding: EdgeInsets.symmetric(horizontal: 12.0),
+    if(widget.requestForm.isActive){
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Course Feedback'),
+          backgroundColor: Colors.indigo.shade300,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(  //gives an overflow error without this
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 16),
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  width: 400.0,
+                  height: 180,
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
+                    color: Colors.indigo.shade50,
                     borderRadius: BorderRadius.circular(8.0),
+
                   ),
-                  child: TextField(
-                    controller: feedbackController,
-                    maxLines: null, // Allow unlimited lines
-                    decoration: InputDecoration(
-                      hintText: 'Enter your feedback here.',
-                      border: InputBorder.none,
-                    ),
+
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 10),
+                      Text(
+                        'Course Code: ${widget.requestForm.code}',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Course Name: ${widget.requestForm.name}',
+                        style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      Text('Topic: ${widget.requestForm.topic}'),
+                      SizedBox(height: 20),
+
+                      //Text('Teacher: ${widget.requestForm.teacherName}'),
+                      //SizedBox(height: 8),
+
+                      Text('Date: ${widget.requestForm.date}'),
+                    ],
                   ),
                 ),
-              ),
 
-*/
+                SizedBox(height: 70),
 
-              SizedBox(height: 20),
-              Align(
-                alignment: Alignment.centerRight,
-                child: ElevatedButton(
-                  onPressed: (){
-                    submitFeedback();
-                  },
-                  //final feedbackText= feedbackController.text;
-
-                  child: Text('Submit'),
+                Text(
+                  'Share your feedback:',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
-              ),
-            ],
+                SizedBox(height: 16),
+
+
+                TextField(
+                  controller: feedbackController,
+                  minLines: 5,
+                  maxLines: 10, //buna bak
+                  decoration: InputDecoration(
+                    hintText: 'Enter your feedback here.',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: ElevatedButton(
+                    onPressed: submitFeedback,
+                    //final feedbackText= feedbackController.text;
+                    child: Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
+
           ),
 
         ),
+      );
+    } else{
+      return Scaffold(
+        body: Center(child: Text('Form Not Available.')),
+      );
+    }
 
-      ),
-    );
   }
 
   Future<void> submitFeedback() async {
     final feedbackText = feedbackController.text;
-    final courseCode = widget.requestForm.code;
+    final formID = widget.requestForm.formId;
 
-    if (feedbackText.trim().isEmpty) {
+    if (feedbackText
+        .trim()
+        .isEmpty) {
       _showErrorSnackBar('Feedback field cannot be empty.');
       return;
     }
 
-    final response = await supabase
-        .schema('ecfs')
-        .from('feedback')
-        .insert({'course_code': courseCode, 'content': feedbackText});
-
-    if (response.error == null) {
-
-      Navigator.pop(context); // Close the feedback page, successful
-    } else {
-      _showErrorSnackBar('Error submitting feedback');
+    try {
+      final response = await supabase
+          .schema('ecfs')
+          .from('feedback')
+          .insert({'form_id': formID, 'content': feedbackText});
+      Navigator.pop(context);
+    } catch (error) {
+      context.showErrorSnackBar(message: unexpectedErrorMessage);
     }
   }
-
-  void _showErrorSnackBar(String message) {
-    context.showErrorSnackBar(message: message);
-  }
+    void _showErrorSnackBar(String message) {
+      context.showErrorSnackBar(message: message);
+    }
 
 
 }
-
